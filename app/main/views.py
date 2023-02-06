7b9441497c143112d7595b4837f7686ab2e43dee
jordan <jordan.thielman@outlook.com>
2023-02-05 20:51:22 -0600

    First application version complete

--- app/main/views.py
+++ app/main/views.py
@@ -1,15 +1,29 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.db.models import Count
+from .models import Trailer, Package
 
 # Create your views here.
 
 def index(response):
     return render(response, "main/base.html", {})
 
-def search(request):
+def index(request):
     if request.method == 'POST':
         input_text = request.POST.get('inputText')
-        # Perform database query using input_text as a parameter
-        # ...
-        return HttpResponse('Query results')
-    return render(request, 'base.html')
+        trailers = Trailer.objects.filter(id=input_text)
+        result = []
+        for trailer in trailers:
+            packages = Package.objects.filter(trailer_id=trailer.id)
+            total_count = packages.count()
+            for package in packages.values('destination_lane').annotate(destination_count=Count('destination_lane')):
+                percentage = round(package['destination_count'] * 100.0 / total_count, 2)
+                result.append({
+                    "destination_lane": package['destination_lane'],
+                    "percentage": percentage,
+                })
+        return render(request, "main/base.html", {"result": result})
+    return render(request, 'main/base.html')
+
+
+
