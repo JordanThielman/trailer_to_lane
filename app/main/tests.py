647fcbd19e39c9ae1646df28bde2191ca9ae5cd3
jordan <jordan.thielman@outlook.com>
2023-02-06 07:02:09 -0600

    Basic testing commit

--- app/main/tests.py
+++ app/main/tests.py
@@ -1,3 +1,22 @@
 from django.test import TestCase
+from django.urls import reverse
+import pytest
+import os
+from .models import Package
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', "trailer_to_lane.settings")
+import django
+django.setup()
 
 # Create your tests here.
+def test_homepage_access():
+    url = reverse('home')
+    assert url == "/"
+
+def test_create_new_package():
+    new_package = Package.objects.create(
+        id=27089,
+        destination_lane=4,
+        size='lrg',
+        trailer_id=13
+    )
+    assert new_package.id == 27089
