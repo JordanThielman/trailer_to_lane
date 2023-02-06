79c957eb83567136ef207dd6803c2d24f92a8e2e
jordan <jordan.thielman@outlook.com>
2023-02-05 21:23:59 -0600

    AWS EC2 settings commit

--- app/trailer_to_lane/settings.py
+++ app/trailer_to_lane/settings.py
@@ -28,7 +28,9 @@ SECRET_KEY = 'django-insecure-xm95hol4&2b66_*g#my*%qx^f*=0^a$--zapb@j$p!9r!%2%2m
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'ec2-3-128-81-130.us-east-2.compute.amazonaws.com', '0.0.0.0', 'localhost', '127.0.0.1'
+]
 
 
 # Application definition
